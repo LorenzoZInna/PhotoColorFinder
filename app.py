19f8ef270a3f8b9b57714fe8bc0969538dc01e1f
Lorenzo Zinna <lorzinna@gmail.com>
2025-05-02 15:06:08 +0200

    saved version that reruns ml process all over again

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ import cv2 as cv
 from cv2 import cvtColor,resize
 import matplotlib.pyplot as plt
 import webbrowser
+import WebAppFunctions
+
+import streamlit as st
+from streamlit_clickable_images import clickable_images
 
 import KnnColorFunctions as Knnfcn
 
 
-st.title("🖼️ Simple Image Transformer")
+st.title("🖼️ KNN Color Finder")
 
 # File uploader
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
@@ -28,3 +32,24 @@ if uploaded_file is not None:
     st.subheader("Decomposed Colors from Image")
     st.pyplot(decomposed_reshaped_image_3dplot, use_container_width=True)
     st.caption("All pixels of the intial image are decomposed into their RGB components and represented in a 3D plot")
+    k_index,centroids_k,labels_k,k_inertia,silhouette_k = Knnfcn.KMeans_models(reshaped_image)
+    red_array,green_array,blue_array = Knnfcn.colors_k_centroids_all (centroids_k)
+    centroid_k_main_index,percentage_counts_k,k_main_color_list = Knnfcn.three_main_centroids_colors(labels_k,red_array,green_array,blue_array)
+    Knnfcn.create_color_rectangle_centroid(k_main_color_list,percentage_counts_k)
+
+    import streamlit as st
+    import os
+
+    image_folder = "images"
+    image_files = sorted([f for f in os.listdir(image_folder) if f.endswith(".png")])
+
+    st.write("Choose an image:")
+
+    for i, filename in enumerate(image_files):
+        col1, col2 = st.columns([1, 2])
+        with col1:
+            st.image(os.path.join(image_folder, filename), width=100)
+        with col2:
+            if st.button(f"Select {filename}", key=filename):
+                st.write(f"You selected: {filename}")
+                st.image(os.path.join(image_folder, filename), caption=filename, use_container_width=True)
